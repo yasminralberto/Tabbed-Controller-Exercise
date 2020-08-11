cdaac19efa46430982ee1d46d092d2f0d79b4c4a
yasminralberto <67976210+yasminralberto@users.noreply.github.com>
2020-08-10 22:44:36 -0500

    Add files via upload

---
+++ Yasmin_A_TableView_Exercise/Yasmin_A_TableView_Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yasmin_A_TableView_Exercise
+//
+//  Created by Yasmin Alberto Ruiz on 8/10/20.
+//  Copyright © 2020 Yasmin Alberto Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
